4856f98f56a1d65153adaf77e40da95240b770f7
ktm000818 <kimtm0818@gmail.com>
2023-04-07 17:37:09 +0900

    Style: app.js 린트 적용

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import socket from 'socket.io';
-import http from 'http';
+import express from "express";
+import socket from "socket.io";
+import http from "http";
 
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
 // express 객체 생성
 const app = express();
@@ -12,45 +12,52 @@ const server = http.createServer(app);
 
 const io = socket(server);
 
-app.use(express.static(path.resolve(__dirname, "..", "chat-ui-react", "dist")))
+app.use(express.static(path.resolve(__dirname, "..", "chat-ui-react", "dist")));
 
 app.get("/", (req, res) => {
-    fs.readFile('../chat-ui-react/dist/index.html', (err, data) => {
-        if (err) {
-            console.error('error');
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.write(data);
-            res.end();
-            console.log("유저가 / 으로 접속했습니다.");
-        }
-    })
-})
+  fs.readFile("../chat-ui-react/dist/index.html", (err, data) => {
+    if (err) {
+      console.error("error");
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+      console.log("유저가 / 으로 접속했습니다.");
+    }
+  });
+});
 
-io.sockets.on('connection', (socket) => {
-    socket.on('newUser', (name) => {
-        console.log(name + " is connected.");
+io.sockets.on("connection", (socket) => {
+  socket.on("newUser", (name) => {
+    console.log(name + " is connected.");
 
-        socket.name = name;
+    socket.name = name;
 
-        io.sockets.emit('update', {type: 'connect', name: 'SERVER', message: name + " is connected"});
-    })
+    io.sockets.emit("update", {
+      type: "connect",
+      name: "SERVER",
+      message: name + " is connected",
+    });
+  });
 
-    socket.on('message', (data) => {
-        data.name = socket.name;
+  socket.on("message", (data) => {
+    data.name = socket.name;
 
-        console.log(data);
+    console.log(data);
 
-        socket.broadcast.emit('update', data);
-    })
+    socket.broadcast.emit("update", data);
+  });
 
-    socket.on('disconnect', () => {
-        console.log(socket.name + " is disconnected");
-        socket.broadcast.emit('update', {type: "disconnect", name: "SERVER", message: socket.name + " is disconnected"})
-    })
-})
+  socket.on("disconnect", () => {
+    console.log(socket.name + " is disconnected");
+    socket.broadcast.emit("update", {
+      type: "disconnect",
+      name: "SERVER",
+      message: socket.name + " is disconnected",
+    });
+  });
+});
 
 server.listen(1234, () => {
-    console.log("서버 실행 중")
-})
-
+  console.log("서버 실행 중");
+});
